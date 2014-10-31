3b9b00f8f45e1c45aa670a4e7df1290a7c6e0465
Guilherme Viebig <guilherme@bitloom.net>
2014-10-31 13:27:05 -0200

    fixed semicolon

--- lib/Base.php
+++ lib/Base.php
@@ -2,7 +2,7 @@
 
 namespace Viebig\CobreGratis;
 
-use Viebig\CobreGratis\ActiveResource as ActiveResource
+use Viebig\CobreGratis\ActiveResource as ActiveResource;
 
 class Base extends ActiveResource {
   var $site = 'https://app.cobregratis.com.br/';
